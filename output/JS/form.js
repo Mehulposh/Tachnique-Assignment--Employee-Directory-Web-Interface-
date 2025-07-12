dd9cd5d540baf9e8e03b99b4f29d213cfc67f6b6
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-12 14:15:58 +0530

    added comments to form.js file

--- output/JS/form.js
+++ output/JS/form.js
@@ -1,11 +1,14 @@
-const form = document.getElementById('employeeForm');
+const form = document.getElementById('employeeForm');//accessing the  dom element
 
 
 document.addEventListener('DOMContentLoaded' , () => {
+    //method to get id from url
     const params = new URLSearchParams(window.location.search);
     const id = params.get('id');
+    console.log(id);
 
     if(id){
+        //promise array for resolving the multiple promises
         Promise.all([
             fetch('../EmployeeData.json').then(res => res.json()),
             Promise.resolve(JSON.parse(localStorage.getItem('newEmployees') || '[]'))
@@ -17,7 +20,7 @@ document.addEventListener('DOMContentLoaded' , () => {
 
                 const employee = fullData.find(emp => emp.id === id);
 
-
+                //auto completeing the form with existing data 
                 if(employee){
                     form.elements['id'].value = employee.id;
                     form.elements['firstName'].value = employee.firstName;
@@ -41,6 +44,7 @@ document.addEventListener('DOMContentLoaded' , () => {
     form.addEventListener('submit' , (e) => {
         e.preventDefault();
 
+        //creating an employee object with data from form
         const employee = {
             id: form.elements['id'].value || Date.now(),  
             firstName: form.elements['firstName'].value.trim(),
@@ -50,18 +54,19 @@ document.addEventListener('DOMContentLoaded' , () => {
             role: form.elements['role'].value.trim()
         }
 
+        //retrieving from local storage
         const newEmployees = JSON.parse(localStorage.getItem('newEmployees') || '[]');
         
         // check if exists in newEmployees
         const idx = newEmployees.findIndex(emp => String(emp.id) === String(employee.id));
 
         if (idx !== -1) {
-            newEmployees[idx] = employee; // update existing
+            newEmployees[idx] = employee; // update existing data
         } else {
-            newEmployees.push(employee); // add new
+            newEmployees.push(employee); // add new data
         }
 
-        
+        //storing the new employees data in local storage
         localStorage.setItem('newEmployees', JSON.stringify(newEmployees));
 
 
